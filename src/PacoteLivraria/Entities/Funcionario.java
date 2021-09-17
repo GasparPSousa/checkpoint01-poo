f1da7dfd114206e3bf8bd9f45ca5d871a9c96ab3
GasparPSousa <gasparufrj@gmail.com>
2021-09-17 16:30:39 -0300

    Criando a classe Funcionario

--- src/PacoteLivraria/Entities/Funcionario.java
+++ src/PacoteLivraria/Entities/Funcionario.java
@@ -1,4 +1,42 @@
 package PacoteLivraria.Entities;
 
-public class Funcionario {
+public abstract class Funcionario {
+
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+    private String email;
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, String sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public abstract void calcularPagamento();
+
+    @Override
+    public String toString() {
+        return  nome
+                + ' '
+                + sobrenome;
+    }
 }
